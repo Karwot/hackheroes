76e482de3ac083007d526f19bb19c1afd7292709
Karwot <33005594+Karwot@users.noreply.github.com>
2017-10-22 19:27:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2018 Kacper Karwot
+Copyright (c) 2017 Kacper Karwot
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
